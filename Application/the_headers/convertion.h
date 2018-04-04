e20d9938a57c4267e4b4c64b16bb0594206a169c
Unknown <solverytom@hotmail.fr>
2018-04-04 22:27:26 +0200

    Revert "Petite modife du main."
    This reverts commit 51e27f4ac0c3a0571d7070079a544c460b97ff2e.

--- Application/the_headers/convertion.h
+++ Application/the_headers/convertion.h
@@ -4,7 +4,7 @@
 #include "projet.h"
 
 rgb pixel_to_rgb(uint64_t);
-image P3_to_P2(image);
+image P3_to_P2(image, char *);
 image binarisation(image, float);
 
 #endif
--- Application/the_sources/main.c
+++ Application/the_sources/main.c
@@ -4,13 +4,10 @@
 #include "projet.h"
 #include "openandsave.h"
 #include "convertion.h"
-#define ALPHA 0.2
 
 int main(int argc,char **argv){
   image imgP3 = P3Lecture(argv[1]);
-  image imgP2 = P3_to_P2(imgP3);
-  image imgP1 = binarisation(imgP3,ALPHA);
+  image imgP2 = P3_to_P2(imgP3, argv[1]);
   save(imgP2,argv[1]);
-  save(imgP1,argv[1]);
 }
 
